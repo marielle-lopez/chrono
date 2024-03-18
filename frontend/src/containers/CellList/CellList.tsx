8b1d2bd14770b4d8ad57f269a111619f2b1058cf
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-18 17:49:39 +1100

    make year and month props of CellList component

--- frontend/src/containers/CellList/CellList.tsx
+++ frontend/src/containers/CellList/CellList.tsx
@@ -1,41 +1,37 @@
 import Cell from '../../components/Cell/Cell';
 
-const getDaysInMonth = (): Date[] => {
-  const currentDate = new Date();
-  const month = currentDate.getMonth();
-  const year = currentDate.getFullYear();
-  const date = new Date(year, month, 1);
-  const days = [];
+const CellList = ({ year, month }: { year: number; month: number }) => {
+  const getDaysInMonth = (year: number, month: number): Date[] => {
+    const date = new Date(year, month, 1);
+    const days = [];
 
-  while (date.getMonth() === month) {
-    days.push(new Date(date));
-    date.setDate(date.getDate() + 1);
-  }
+    while (date.getMonth() === month) {
+      days.push(new Date(date));
+      date.setDate(date.getDate() + 1);
+    }
 
-  return days;
-};
+    return days;
+  };
 
-const startDummyDays = (date: Date): number[] => {
-  const numberArr = [];
-  for (let i = 0; i < date.getDay(); i++) {
-    numberArr.push(i);
-  }
+  const startDummyDays = (date: Date): number[] => {
+    const numberArr = [];
+    for (let i = 0; i < date.getDay(); i++) {
+      numberArr.push(i);
+    }
 
-  return numberArr;
-};
+    return numberArr;
+  };
 
-const endDummyDays = (date: Date): number[] => {
-  const numberArr = [];
-  for (let i = date.getDay() + 1; i <= 6; i++) {
-    console.log(i);
-    numberArr.push(i);
-  }
+  const endDummyDays = (date: Date): number[] => {
+    const numberArr = [];
+    for (let i = date.getDay() + 1; i <= 6; i++) {
+      numberArr.push(i);
+    }
 
-  return numberArr;
-};
+    return numberArr;
+  };
 
-const CellList = () => {
-  const days = getDaysInMonth();
+  const days = getDaysInMonth(year, month);
 
   return (
     <div className="flex-grow grid grid-cols-7 gap-px gap-py px-px py-px bg-stone-800">
