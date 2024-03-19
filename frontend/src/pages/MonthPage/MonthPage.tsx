2abec91b4306ec415ae72fec45e0bc576fea6b38
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-19 12:10:56 +1100

    update from div to main tags to ensure semantic HTML

--- frontend/src/pages/MonthPage/MonthPage.tsx
+++ frontend/src/pages/MonthPage/MonthPage.tsx
@@ -3,10 +3,10 @@ import CellList from '../../containers/CellList/CellList';
 
 const MonthPage = ({ month, year }: { month: number; year: number }) => {
   return (
-    <div className="flex flex-col flex-grow">
+    <main className="flex flex-col flex-grow">
       <CellHeaders />
       {month !== -1 && year !== -1 && <CellList year={year} month={month} />}
-    </div>
+    </main>
   );
 };
 
