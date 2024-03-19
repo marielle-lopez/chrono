dbc5f4dc60ab0c1c5820750d76f220426e7a53c1
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-19 12:12:34 +1100

    update props of Cell to increase its versatility and make it usable for different views

--- frontend/src/components/Cell/Cell.tsx
+++ frontend/src/components/Cell/Cell.tsx
@@ -1,5 +1,25 @@
-const Cell = ({ day }: { day: number }) => {
-  return <div className="flex bg-stone-900">{day !== 0 ? day : ''}</div>;
+type Nullable<T> = T | undefined | null;
+
+interface CellProps {
+  hour?: Nullable<number>;
+  day?: Nullable<number>;
+}
+
+const Cell = ({ hour = null, day = null }: CellProps) => {
+  return (
+    <>
+      {hour || hour === 0 ? (
+        <div className="flex-grow bg-stone-900">{hour}</div>
+      ) : (
+        ''
+      )}
+      {day || day === 0 ? (
+        <div className="flex bg-stone-900">{day !== 0 ? day : ''}</div>
+      ) : (
+        ''
+      )}
+    </>
+  );
 };
 
 export default Cell;
