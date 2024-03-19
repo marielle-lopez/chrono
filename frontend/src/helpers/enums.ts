55ba08acff9194b4486c63f91c6d6a085de99919
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-19 12:10:03 +1100

    add Day enum to help convert from day numbers

--- frontend/src/helpers/enums.ts
+++ frontend/src/helpers/enums.ts
@@ -12,3 +12,13 @@ export enum Month {
   November = 10,
   December = 11,
 }
+
+export enum Day {
+  Sunday = 0,
+  Monday = 1,
+  Tuesday = 2,
+  Wednesday = 3,
+  Thursday = 4,
+  Friday = 5,
+  Saturday = 6,
+}
