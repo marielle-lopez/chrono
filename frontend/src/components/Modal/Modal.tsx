dc683e02ee7a64e48f9ca5e9389e17a2f40d0f42
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-20 22:20:02 +1100

    set up skeletal content structure

--- frontend/src/components/Modal/Modal.tsx
+++ frontend/src/components/Modal/Modal.tsx
@@ -1,5 +1,23 @@
+import { FaXmark } from 'react-icons/fa6';
+
 const Modal = () => {
-  return <div>Modal</div>;
+  return (
+    <div className="flex flex-col">
+      <div className="flex justify-between">
+        <h2>Modal Heading</h2>
+        <button>
+          <FaXmark />
+        </button>
+      </div>
+      <div>
+        <p>Modal Body</p>
+      </div>
+      <div className="flex justify-end gap-4">
+        <button>Cancel button</button>
+        <button>Submit button</button>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
