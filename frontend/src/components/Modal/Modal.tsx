0fb3365f8f25c024b2b273afae9849673bf9c954
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-20 22:48:58 +1100

    construct Modal with underlaying overlay, add basic styling, and implement conditional rendering to display Modal component based on isHidden prop

--- frontend/src/components/Modal/Modal.tsx
+++ frontend/src/components/Modal/Modal.tsx
@@ -1,22 +1,46 @@
 import { FaXmark } from 'react-icons/fa6';
 
-const Modal = () => {
+interface ModalProps {
+  isHidden: boolean;
+  setIsHidden: (isHidden: boolean) => void;
+}
+
+const Modal = ({ isHidden, setIsHidden }: ModalProps) => {
   return (
-    <div className="flex flex-col">
-      <div className="flex justify-between">
-        <h2>Modal Heading</h2>
-        <button>
-          <FaXmark />
-        </button>
+    <>
+      <div
+        className={`w-full h-full fixed backdrop-blur-sm ${
+          isHidden && 'hidden'
+        }`}
+      ></div>
+      <div
+        className={`flex w-full h-full fixed justify-center items-center ${
+          isHidden && 'hidden'
+        }`}
+      >
+        <div className="flex flex-col bg-black rounded-md px-3 py-2">
+          <div className="flex justify-between">
+            <h2>Modal Heading</h2>
+            <button onClick={() => setIsHidden(true)}>
+              <FaXmark />
+            </button>
+          </div>
+          <div>
+            <p>Modal Body</p>
+          </div>
+          <div className="flex justify-end gap-4">
+            <button onClick={() => setIsHidden(true)}>Cancel button</button>
+            <button
+              onClick={() =>
+                console.log('Submit button clicked in Modal component')
+              }
+            >
+              Submit button
+            </button>
+          </div>
+        </div>
       </div>
-      <div>
-        <p>Modal Body</p>
-      </div>
-      <div className="flex justify-end gap-4">
-        <button>Cancel button</button>
-        <button>Submit button</button>
-      </div>
-    </div>
+    </>
   );
 };
 
