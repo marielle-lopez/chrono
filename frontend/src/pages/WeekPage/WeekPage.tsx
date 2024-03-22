aa6314e75b204e678b6549d06e3d2461794a0085
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-22 11:38:45 +1100

    implement CellList component to render days of week

--- frontend/src/pages/WeekPage/WeekPage.tsx
+++ frontend/src/pages/WeekPage/WeekPage.tsx
@@ -1,5 +1,25 @@
+import CellList from '../../containers/CellList/CellList';
+// import { formatDateToString } from '../../helpers/functions';
+
 const WeekPage = ({ day }: { day: Date }) => {
-  return <main>WeekPage: {day.toISOString()}</main>;
+  // const getDatesInWeek = (date: Date): Date[] => {
+  //   return Array(7)
+  //     .fill(new Date(date))
+  //     .map(
+  //       (date, i) => new Date(date.setDate(date.getDate() - date.getDay() + i))
+  //     );
+  // };
+
+  // const datesInWeek = getDatesInWeek(day);
+  // console.log(datesInWeek);
+
+  return (
+    <main className="flex flex-grow">
+      {/* <p>{formatDateToString(datesInWeek[0])}</p>
+      <p>{formatDateToString(datesInWeek[datesInWeek.length - 1])}</p> */}
+      <CellList view="week" day={day} />
+    </main>
+  );
 };
 
 export default WeekPage;
