44b9764b9ed001b996ac0aac82f59957c233d3a6
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-22 16:15:12 +1100

    adjust padding and add `type` prop to modify styling depending on value of `type`

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -2,6 +2,7 @@ type Nullable<T> = T | undefined | null;
 
 interface ButtonProps {
   children?: Nullable<any>;
+  type?: Nullable<string>;
   label?: Nullable<string>;
   icon?: Nullable<Image>;
   handleClick: () => any;
@@ -9,13 +10,16 @@ interface ButtonProps {
 
 const Button = ({
   children = null,
+  type = null,
   label = null,
   icon = null,
   handleClick,
 }: ButtonProps) => {
   return (
     <button
-      className="border border-stone-700 rounded-md bg-stone-800"
+      className={`py-1 border border-stone-700 rounded-md bg-stone-800 ${
+        label !== null ? 'px-3' : 'px-1'
+      } ${type === 'primary' && 'bg-lime-400 border-none text-black'}`}
       onClick={handleClick}
     >
       {children && children}
