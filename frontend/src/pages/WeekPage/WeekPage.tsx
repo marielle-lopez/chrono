4f955f58c3c47b8f6183272a6c703eef9085881e
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-22 12:14:32 +1100

    remove commented code

--- frontend/src/pages/WeekPage/WeekPage.tsx
+++ frontend/src/pages/WeekPage/WeekPage.tsx
@@ -1,22 +1,8 @@
 import CellList from '../../containers/CellList/CellList';
-// import { formatDateToString } from '../../helpers/functions';
 
 const WeekPage = ({ day }: { day: Date }) => {
-  // const getDatesInWeek = (date: Date): Date[] => {
-  //   return Array(7)
-  //     .fill(new Date(date))
-  //     .map(
-  //       (date, i) => new Date(date.setDate(date.getDate() - date.getDay() + i))
-  //     );
-  // };
-
-  // const datesInWeek = getDatesInWeek(day);
-  // console.log(datesInWeek);
-
   return (
     <main className="flex flex-grow">
-      {/* <p>{formatDateToString(datesInWeek[0])}</p>
-      <p>{formatDateToString(datesInWeek[datesInWeek.length - 1])}</p> */}
       <CellList view="week" day={day} />
     </main>
   );
