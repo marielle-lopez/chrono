2ac3aa258280f0165a1252b897147bafadaa8a1e
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-22 16:13:05 +1100

    update styling and implement `Button` component

--- frontend/src/components/Modal/Modal.tsx
+++ frontend/src/components/Modal/Modal.tsx
@@ -1,4 +1,5 @@
 import { FaXmark } from 'react-icons/fa6';
+import Button from '../Button/Button';
 
 interface ModalProps {
   isHidden: boolean;
@@ -18,25 +19,27 @@ const Modal = ({ isHidden, setIsHidden }: ModalProps) => {
           isHidden && 'hidden'
         }`}
       >
-        <div className="flex flex-col bg-black rounded-md px-3 py-2">
-          <div className="flex justify-between">
-            <h2>Modal Heading</h2>
-            <button onClick={() => setIsHidden(true)}>
-              <FaXmark />
-            </button>
+        <div className="flex flex-col justify-between w-96 h-60 bg-black rounded-lg px-4 py-4 shadow-xl">
+          <div className="flex flex-col gap-2">
+            <div className="flex justify-between">
+              <h2 className="text-xl font-bold">Modal Heading</h2>
+              <button onClick={() => setIsHidden(true)}>
+                <FaXmark />
+              </button>
+            </div>
+            <div>
+              <p>Modal Body</p>
+            </div>
           </div>
-          <div>
-            <p>Modal Body</p>
-          </div>
-          <div className="flex justify-end gap-4">
-            <button onClick={() => setIsHidden(true)}>Cancel button</button>
-            <button
-              onClick={() =>
-                console.log('Submit button clicked in Modal component')
+          <div className="flex justify-end gap-3">
+            <Button handleClick={() => setIsHidden(true)} label="Cancel" />
+            <Button
+              handleClick={() =>
+                console.log('Primary button clicked in Modal component')
               }
-            >
-              Submit button
-            </button>
+              label="Primary"
+              type="primary"
+            />
           </div>
         </div>
       </div>
