f4c6239459feadb41206f3a25e5d4dda320af361
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-22 12:14:47 +1100

    remove unnecessary heading

--- frontend/src/pages/DayPage/DayPage.tsx
+++ frontend/src/pages/DayPage/DayPage.tsx
@@ -1,13 +1,8 @@
 import CellList from '../../containers/CellList/CellList';
-import { Day, Month } from '../../helpers/enums';
 
 const DayPage = ({ day }: { day: Date }) => {
   return (
     <main className="flex flex-col flex-grow">
-      <h1>
-        {Object.values(Day)[day.getDay()]} {day.getDate()}{' '}
-        {Object.values(Month)[day.getMonth()]} {day.getFullYear()}
-      </h1>
       <CellList view="day" day={day} />
     </main>
   );
