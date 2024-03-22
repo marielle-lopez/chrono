52bf78ebc106c9535b3809399bd27cf7995b51c3
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-22 11:41:57 +1100

    add commonly used `formatDateToString()` function

--- frontend/src/helpers/functions.ts
+++ frontend/src/helpers/functions.ts
@@ -1,3 +1,14 @@
+import { Day, Month } from './enums';
+
 export const daysInMonth = (month: number, year: number) => {
   return new Date(year, month, 0).getDate();
+};
+
+export const formatDateToString = (date: Date): string => {
+  const day = Object.values(Day)[date.getDay()];
+  const dateNum = date.getDate();
+  const month = Object.values(Month)[date.getMonth()];
+  const year = date.getFullYear();
+
+  return `${day} ${dateNum} ${month} ${year}`;
 };
