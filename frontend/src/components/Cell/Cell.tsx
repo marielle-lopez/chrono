5f97009725d595c141336231a9f0829d37d71014
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-22 15:32:06 +1100

    add `setIsHidden()` function as prop and make `Cell` components clickable to open `Modal` component if not a dummy cell

--- frontend/src/components/Cell/Cell.tsx
+++ frontend/src/components/Cell/Cell.tsx
@@ -1,24 +1,35 @@
 type Nullable<T> = T | undefined | null;
 
 interface CellProps {
+  setIsHidden?: Nullable<(isHidden: boolean) => void>;
   hour?: Nullable<number>;
   day?: Nullable<number>;
 }
 
-const Cell = ({ hour = null, day = null }: CellProps) => {
+const Cell = ({ setIsHidden = null, hour = null, day = null }: CellProps) => {
+  console.log(hour);
   return (
     <>
-      {hour || hour === 0 ? (
-        <div className="flex-grow bg-stone-900">{hour}</div>
-      ) : (
-        ''
+      {(hour || hour === 0) && setIsHidden && (
+        <div
+          className="flex-grow bg-stone-900"
+          onClick={() => setIsHidden(false)}
+        >
+          {hour}
+        </div>
       )}
-      {day || day === 0 ? (
+      {(day || day === 0) && setIsHidden === null && (
         <div className="flex flex-grow bg-stone-900">
           {day !== 0 ? day : ''}
         </div>
-      ) : (
-        ''
+      )}
+      {(day || day === 0) && setIsHidden && (
+        <div
+          className="flex flex-grow bg-stone-900"
+          onClick={() => setIsHidden(false)}
+        >
+          {day !== 0 ? day : ''}
+        </div>
       )}
     </>
   );
