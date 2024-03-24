f6e44bc3cfa8350d2970f36ecb455e04c37428b7
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-24 21:07:41 +1100

    create form in `EventForm` component and add styling

--- frontend/src/components/EventForm/EventForm.tsx
+++ frontend/src/components/EventForm/EventForm.tsx
@@ -4,34 +4,66 @@ const EventForm = () => {
   const { register } = useForm();
 
   return (
-    <form>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" {...register('name')} />
-      </div>
-
-      <div>
-        <label htmlFor="startDate">Start Date</label>
+    <form className="flex flex-col flex-grow gap-3">
+      <div className="flex flex-col gap-1">
+        <label className="text-sm font-bold" htmlFor="name">
+          Name
+        </label>
         <input
-          type="datetime-local"
-          id="startDate"
-          {...register('startDate')}
+          className="flex-grow bg-stone-900 border border-stone-800 rounded-md"
+          type="text"
+          id="name"
+          {...register('name')}
         />
       </div>
 
-      <div>
-        <label htmlFor="endDate">End Date</label>
-        <input type="datetime-local" id="endDate" {...register('endDate')} />
+      <div className="flex gap-2">
+        <div className="flex flex-col gap-1">
+          <label className="text-sm font-bold" htmlFor="startDate">
+            Start Date
+          </label>
+          <input
+            className="bg-stone-900 border border-stone-800 rounded-md px-1.5"
+            type="datetime-local"
+            id="startDate"
+            {...register('startDate')}
+          />
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <label className="text-sm font-bold" htmlFor="endDate">
+            End Date
+          </label>
+          <input
+            className="bg-stone-900 border border-stone-800 rounded-md px-1.5"
+            type="datetime-local"
+            id="endDate"
+            {...register('endDate')}
+          />
+        </div>
       </div>
 
-      <div>
-        <label htmlFor="location">Location</label>
-        <input type="text" id="location" {...register('location')} />
+      <div className="flex flex-col gap-1">
+        <label className="text-sm font-bold" htmlFor="location">
+          Location
+        </label>
+        <input
+          className="flex-grow bg-stone-900 border border-stone-800 rounded-md"
+          type="text"
+          id="location"
+          {...register('location')}
+        />
       </div>
 
-      <div>
-        <label htmlFor="label">Label</label>
-        <select id="label" {...register('label')}>
+      <div className="flex flex-col gap-1">
+        <label className="text-sm font-bold" htmlFor="label">
+          Label
+        </label>
+        <select
+          className="flex-grow bg-stone-900 border border-stone-800 rounded-md px-1"
+          id="label"
+          {...register('label')}
+        >
           <option>Label 1</option>
           <option>Label 2</option>
           <option>Label 3</option>
