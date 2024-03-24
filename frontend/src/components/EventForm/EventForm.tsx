d32994946ba84d23bb07df300e32372ea35124c4
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-24 22:11:56 +1100

    accept `formSubmit` prop, use Zod to handle form validation and React Hook Form for form submission

--- frontend/src/components/EventForm/EventForm.tsx
+++ frontend/src/components/EventForm/EventForm.tsx
@@ -1,73 +1,121 @@
+import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
+import { schema } from './schema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import Button from '../Button/Button';
 
-const EventForm = () => {
-  const { register } = useForm();
+const EventForm = ({ formSubmit }) => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitSuccessful },
+  } = useForm({ resolver: zodResolver(schema) });
+
+  useEffect(() => {
+    reset();
+    console.log('Hello world!');
+  }, [isSubmitSuccessful]);
 
   return (
-    <form className="flex flex-col flex-grow gap-3">
-      <div className="flex flex-col gap-1">
-        <label className="text-sm font-bold" htmlFor="name">
-          Name
-        </label>
-        <input
-          className="flex-grow bg-stone-900 border border-stone-800 rounded-md"
-          type="text"
-          id="name"
-          {...register('name')}
-        />
-      </div>
-
-      <div className="flex gap-2">
-        <div className="flex flex-col gap-1">
-          <label className="text-sm font-bold" htmlFor="startDate">
-            Start Date
+    <form
+      className="flex flex-col flex-grow gap-8"
+      onSubmit={handleSubmit(formSubmit)}
+    >
+      <div className="flex flex-col flex-grow gap-4">
+        <div className="flex flex-col gap-2">
+          <label className="text-sm font-bold" htmlFor="name">
+            Name
           </label>
           <input
-            className="bg-stone-900 border border-stone-800 rounded-md px-1.5"
-            type="datetime-local"
-            id="startDate"
-            {...register('startDate')}
+            className="flex-grow bg-stone-900 border border-stone-800 rounded-md px-1.5 py-0.5"
+            type="text"
+            id="name"
+            {...register('name')}
           />
+          {errors.name?.message && (
+            <p className="text-red-500 text-xs">
+              {errors.name.message.toString()}
+            </p>
+          )}
         </div>
 
-        <div className="flex flex-col gap-1">
-          <label className="text-sm font-bold" htmlFor="endDate">
-            End Date
+        <div className="flex gap-2">
+          <div className="flex flex-col gap-2">
+            <label className="text-sm font-bold" htmlFor="startDate">
+              Start Date
+            </label>
+            <input
+              className="bg-stone-900 border border-stone-800 rounded-md px-1.5 py-0.5"
+              type="datetime-local"
+              id="startDate"
+              {...register('startDate')}
+            />
+            {errors.startDate?.message && (
+              <p className="text-red-500 text-xs">
+                {errors.startDate.message.toString()}
+              </p>
+            )}
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <label className="text-sm font-bold" htmlFor="endDate">
+              End Date
+            </label>
+            <input
+              className="bg-stone-900 border border-stone-800 rounded-md px-1.5 py-0.5"
+              type="datetime-local"
+              id="endDate"
+              {...register('endDate')}
+            />
+            {errors.endDate?.message && (
+              <p className="text-red-500 text-xs">
+                {errors.endDate.message.toString()}
+              </p>
+            )}
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <label className="text-sm font-bold" htmlFor="location">
+            Location
           </label>
           <input
-            className="bg-stone-900 border border-stone-800 rounded-md px-1.5"
-            type="datetime-local"
-            id="endDate"
-            {...register('endDate')}
+            className="flex-grow bg-stone-900 border border-stone-800 rounded-md px-1.5 py-0.5"
+            type="text"
+            id="location"
+            {...register('location')}
           />
+          {errors.location?.message && (
+            <p className="text-red-500 text-xs">
+              {errors.location.message.toString()}
+            </p>
+          )}
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <label className="text-sm font-bold" htmlFor="label">
+            Label
+          </label>
+          <select
+            className="flex-grow bg-stone-900 border border-stone-800 rounded-md px-1 py-0.5"
+            id="label"
+            {...register('label')}
+          >
+            <option>Personal</option>
+            <option>Work</option>
+            <option>University</option>
+          </select>
+          {errors.label?.message && (
+            <p className="text-red-500 text-xs">
+              {errors.label.message.toString()}
+            </p>
+          )}
         </div>
       </div>
 
-      <div className="flex flex-col gap-1">
-        <label className="text-sm font-bold" htmlFor="location">
-          Location
-        </label>
-        <input
-          className="flex-grow bg-stone-900 border border-stone-800 rounded-md"
-          type="text"
-          id="location"
-          {...register('location')}
-        />
-      </div>
-
-      <div className="flex flex-col gap-1">
-        <label className="text-sm font-bold" htmlFor="label">
-          Label
-        </label>
-        <select
-          className="flex-grow bg-stone-900 border border-stone-800 rounded-md px-1"
-          id="label"
-          {...register('label')}
-        >
-          <option>Label 1</option>
-          <option>Label 2</option>
-          <option>Label 3</option>
-        </select>
+      <div className="flex justify-end">
+        <Button label="Submit" handleClick={formSubmit} type="primary" />
       </div>
     </form>
   );
