949f7f993617a9fec7f0cb388b6748102a1689f3
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-26 00:08:39 +1100

    remove commented TSX, rearrange layout of content in `NavBar` component, add `select` element for filtering events, accept `setFilter` prop

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom';
 
 interface NavBarProps {
   label: string;
+  setFilter: (filter: string) => void;
   handleDecrement: () => void;
   handleIncrement: () => void;
   switchToDayView: () => void;
@@ -13,6 +14,7 @@ interface NavBarProps {
 
 const NavBar = ({
   label,
+  setFilter,
   handleDecrement,
   handleIncrement,
   switchToDayView,
@@ -21,61 +23,68 @@ const NavBar = ({
 }: NavBarProps) => {
   return (
     <div className="flex justify-between items-center mb-6">
-      <div className="flex w-1/3">
-        {/* <img
-          className="h-6"
-          src="./src/assets/logo/logo-lg.png"
-          alt="Chrono logo"
-        /> */}
+      <div className="flex gap-4">
         <img
           className="h-6"
           src="./src/assets/logo/logo-thumbnail.png"
           alt="Chrono thumbnail logo"
         />
+        <div className="flex justify-center items-center gap-2">
+          <Button handleClick={handleDecrement}>
+            <FaChevronLeft className="h-3" />
+          </Button>
+          <p className="flex text-center items-center">{label}</p>
+          <Button handleClick={handleIncrement}>
+            <FaChevronRight className="h-3" />
+          </Button>
+        </div>
       </div>
-      <div className="flex justify-center items-center w-1/3 gap-2">
-        <Button handleClick={handleDecrement}>
-          <FaChevronLeft className="h-3" />
-        </Button>
-        <p className="flex text-center items-center">{label}</p>
-        <Button handleClick={handleIncrement}>
-          <FaChevronRight className="h-3" />
-        </Button>
-      </div>
-      <div className="flex justify-end w-1/3 gap-2">
-        <NavLink
-          to="/day"
-          className={({ isActive }) =>
-            isActive
-              ? 'bg-lime-400 text-black text-sm px-1.5 rounded-md'
-              : 'text-sm'
-          }
-          onClick={switchToDayView}
+
+      <div className="flex flex-grow justify-end items-center gap-4">
+        <select
+          className="bg-stone-900 border border-stone-800 rounded-md"
+          onChange={(e) => setFilter(e.target.value)}
         >
-          Day
-        </NavLink>
-        <NavLink
-          to="/week"
-          className={({ isActive }) =>
-            isActive
-              ? 'bg-lime-400 text-black text-sm px-1.5 rounded-md'
-              : 'text-sm'
-          }
-          onClick={switchToWeekView}
-        >
-          Week
-        </NavLink>
-        <NavLink
-          to="/month"
-          className={({ isActive }) =>
-            isActive
-              ? 'bg-lime-400 text-black text-sm px-1.5 rounded-md'
-              : 'text-sm'
-          }
-          onClick={switchToMonthView}
-        >
-          Month
-        </NavLink>
+          <option value="All">All</option>
+          <option value="Personal">Personal</option>
+          <option value="University">University</option>
+          <option value="Work">Work</option>
+        </select>
+        <div className="flex gap-2">
+          <NavLink
+            to="/day"
+            className={({ isActive }) =>
+              isActive
+                ? 'bg-lime-400 text-black text-sm px-1.5 rounded-md'
+                : 'text-sm'
+            }
+            onClick={switchToDayView}
+          >
+            Day
+          </NavLink>
+          <NavLink
+            to="/week"
+            className={({ isActive }) =>
+              isActive
+                ? 'bg-lime-400 text-black text-sm px-1.5 rounded-md'
+                : 'text-sm'
+            }
+            onClick={switchToWeekView}
+          >
+            Week
+          </NavLink>
+          <NavLink
+            to="/month"
+            className={({ isActive }) =>
+              isActive
+                ? 'bg-lime-400 text-black text-sm px-1.5 rounded-md'
+                : 'text-sm'
+            }
+            onClick={switchToMonthView}
+          >
+            Month
+          </NavLink>
+        </div>
       </div>
     </div>
   );
