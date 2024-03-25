52832c4425f01778055162d307ff173b544de802
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-25 14:19:35 +1100

    accept `events` prop and pass `events` to `CellList` component

--- frontend/src/pages/WeekPage/WeekPage.tsx
+++ frontend/src/pages/WeekPage/WeekPage.tsx
@@ -5,16 +5,22 @@ import MainWrapper from '../../containers/MainWrapper/MainWrapper';
 const WeekPage = ({
   setIsHidden,
   day,
+  events,
 }: {
   setIsHidden: (isHidden: boolean) => void;
   day: Date;
+  events: Event[];
 }) => {
   return (
     <MainWrapper>
       <CellHeaders />
-      <CellList setIsHidden={setIsHidden} view="week" day={day} />
+      <CellList
+        setIsHidden={setIsHidden}
+        view="week"
+        day={day}
+        events={events}
+      />
     </MainWrapper>
-
   );
 };
 
