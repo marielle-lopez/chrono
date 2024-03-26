89b1315a0ce72c8ca8b96a34f8e837448191ac38
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-27 01:44:23 +1100

    fix dynamic colour styling bug

--- frontend/src/components/EventCard/EventCard.tsx
+++ frontend/src/components/EventCard/EventCard.tsx
@@ -5,27 +5,38 @@ const EventCard = ({
   event: Event;
   setEvent: (event: Event) => void;
 }) => {
+  let containerStyles: string = '';
+  let textStyles: string = '';
+
   const label = () => {
-    if (event.label === 'Personal') {
-      return 'lime';
+    if (event.label === 'personal') {
+      containerStyles =
+        'bg-lime-400 bg-opacity-20 px-2 py-1 rounded-tr-lg rounded-br-lg border-l-4 border-lime-400 hover:bg-black';
+      textStyles = 'text-lime-200 font-medium text-sm';
+      return;
+    } else if (event.label === 'work') {
+      containerStyles =
+        'bg-teal-400 bg-opacity-20 px-2 py-1 rounded-tr-lg rounded-br-lg border-l-4 border-teal-400 hover:bg-black';
+      textStyles = 'text-teal-200 font-medium text-sm';
+      return;
+    } else if (event.label === 'university') {
+      containerStyles =
+        'bg-violet-400 bg-opacity-20 px-2 py-1 rounded-tr-lg rounded-br-lg border-l-4 border-violet-400 hover:bg-black';
+      textStyles = 'text-violet-200 font-medium text-sm';
+      return;
+    } else {
+      containerStyles =
+        'bg-blue-400 bg-opacity-20 px-2 py-1 rounded-tr-lg rounded-br-lg border-l-4 border-blue-400 hover:bg-black';
+      textStyles = 'text-blue-200 font-medium text-sm';
+      return;
     }
-    if (event.label === 'Work') {
-      return 'teal';
-    }
-    if (event.label === 'University') {
-      return 'violet';
-    }
-    return 'blue';
   };
 
-  const colour = label();
+  label();
 
   return (
-    <div
-      className={`bg-${colour}-400 bg-opacity-20 px-2 py-1 rounded-tr-lg rounded-br-lg border-l-4 border-${colour}-400 hover:bg-black`}
-      onClick={() => setEvent(event)}
-    >
-      <p className={`text-${colour}-200 font-medium text-sm`}>{event.name}</p>
+    <div className={containerStyles} onClick={() => setEvent(event)}>
+      <p className={textStyles}>{event.name}</p>
       <p className="uppercase text-xs font-semibold opacity-30">
         {event.startedAt.toLocaleTimeString()}
       </p>
