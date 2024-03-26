f8130dd7e40e1711395d6b4ed602d3ee90f06b57
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-26 11:55:41 +1100

    remove unnecessary `div` containers, update border radius

--- frontend/src/components/EventCard/EventCard.tsx
+++ frontend/src/components/EventCard/EventCard.tsx
@@ -22,21 +22,13 @@ const EventCard = ({
 
   return (
     <div
-      className={`bg-${colour}-400 bg-opacity-20 px-2 py-1 rounded-sm hover:bg-black`}
+      className={`bg-${colour}-400 bg-opacity-20 px-2 py-1 rounded-tr-lg rounded-br-lg border-l-4 border-${colour}-400 hover:bg-black`}
       onClick={() => setEvent(event)}
     >
-      <div
-        className={`border-l-2 border-dotted border-${colour}-400 border-opacity-50`}
-      >
-        <div className="pl-1">
-          <p className={`text-${colour}-200 font-medium text-sm`}>
-            {event.name}
-          </p>
-          <p className="uppercase text-xs font-semibold opacity-30">
-            {event.startDate.toLocaleTimeString()}
-          </p>
-        </div>
-      </div>
+      <p className={`text-${colour}-200 font-medium text-sm`}>{event.name}</p>
+      <p className="uppercase text-xs font-semibold opacity-30">
+        {event.startDate.toLocaleTimeString()}
+      </p>
     </div>
   );
 };
