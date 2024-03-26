66034da8dee5360a9ed7e5f1398c96341109d77b
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-26 16:25:32 +1100

    fully implement methods utilised by `LabelController` layer

--- backend/src/main/java/marielle/lopez/chrono/labels/LabelService.java
+++ backend/src/main/java/marielle/lopez/chrono/labels/LabelService.java
@@ -1,33 +1,56 @@
 package marielle.lopez.chrono.labels;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 
 @Service
 @Transactional
 public class LabelService {
 	@Autowired
+	private ModelMapper modelMapper;
+	
+	@Autowired
 	private LabelRepository labelRepository;
 	
-	public String getAllLabels() {
-		return "This gets all labels";
+	public List<Label> getAllLabels() {
+		return this.labelRepository.findAll();
 	}
 	
-	public String getLabelById(Long id) {
-		return String.format("This gets a label with ID %d", id);
+	public Optional<Label> getLabelById(Long id) {
+		return this.labelRepository.findById(id);
 	}
 	
-	public String createLabel() {
-		return "This creates a label";
+	public Label createLabel(@Valid CreateLabelDTO data) {
+		Label createdLabel = modelMapper.map(data, Label.class);
+		return this.labelRepository.save(createdLabel);
 	}
 	
-	public String updateLabelById(Long id) {
-		return String.format("This updates a label with ID %d", id);
+	public Optional<Label> updateLabelById(Long id, @Valid UpdateLabelDTO data) {
+		Optional<Label> maybeLabel = this.labelRepository.findById(id);
+		if (maybeLabel.isEmpty()) {
+			return maybeLabel;
+		}
+		Label foundLabel = maybeLabel.get();
+		if (data.getName() != null) {
+			foundLabel.setName(data.getName().trim().toLowerCase());
+		}
+		Label updatedLabel = this.labelRepository.save(foundLabel);
+		return Optional.of(updatedLabel);
 	}
 	
-	public String deleteLabelById(Long id) {
-		return String.format("This deletes a label with ID %d", id);
+	public boolean deleteLabelById(Long id) {
+		Optional<Label> maybeLabel = this.labelRepository.findById(id);
+		if (maybeLabel.isEmpty()) {
+			return false;
+		}
+		this.labelRepository.delete(maybeLabel.get());
+		return true;
 	}
 }
