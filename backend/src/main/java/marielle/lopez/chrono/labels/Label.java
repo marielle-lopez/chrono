9f5b63541207f1c8db6379515eb0935256c04e92
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-26 16:24:12 +1100

    update `Label` entity with properties, constructor, and setter method

--- backend/src/main/java/marielle/lopez/chrono/labels/Label.java
+++ backend/src/main/java/marielle/lopez/chrono/labels/Label.java
@@ -1,9 +1,23 @@
 package marielle.lopez.chrono.labels;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import marielle.lopez.chrono.common.BaseEntity;
 
 @Entity
 public class Label extends BaseEntity {
-
+	@Column
+	private String name;
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public Label() {
+		super();
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
 }
