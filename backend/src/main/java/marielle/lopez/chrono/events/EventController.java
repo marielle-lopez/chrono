96402c2cc5790689543774286dbb10261bd77c54
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-26 14:11:29 +1100

    fully implement methods handling `GET` request for all events, and `POST` request

--- backend/src/main/java/marielle/lopez/chrono/events/EventController.java
+++ backend/src/main/java/marielle/lopez/chrono/events/EventController.java
@@ -1,13 +1,20 @@
 package marielle.lopez.chrono.events;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/events")
@@ -16,8 +23,9 @@ public class EventController {
 	private EventService eventService;
 	
 	@GetMapping
-	public String getAllEvents() {
-		return this.eventService.getAllEvents();
+	public ResponseEntity<List<Event>> getAllEvents() {
+		List<Event> allEvents = this.eventService.getAllEvents();
+		return new ResponseEntity<>(allEvents, HttpStatus.OK);
 	}
 	
 	@GetMapping("/{id}")
@@ -26,8 +34,9 @@ public class EventController {
 	}
 	
 	@PostMapping
-	public String createEvent() {
-		return this.eventService.createEvent();
+	public ResponseEntity<Event> createEvent(@Valid @RequestBody CreateEventDTO data) {
+		Event createdEvent = this.eventService.createEvent(data);
+		return new ResponseEntity<>(createdEvent, HttpStatus.CREATED);
 	}
 	
 	@PatchMapping("/{id}")
