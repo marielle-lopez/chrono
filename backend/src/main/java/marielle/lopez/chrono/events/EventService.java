ab8e7d758e78e6db4d8e9d806cf43e1deaf27087
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-26 14:13:40 +1100

    create instantiation of `ModelMapper`, use mapper to transform valid data in `createEvent` method, save valid event data using repository, return all events from repository in `getAllEvents()` method

--- backend/src/main/java/marielle/lopez/chrono/events/EventService.java
+++ backend/src/main/java/marielle/lopez/chrono/events/EventService.java
@@ -2,27 +2,34 @@ package marielle.lopez.chrono.events;
 
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 
 @Service
 @Transactional
 public class EventService {
 	@Autowired
+	private ModelMapper modelMapper;
+	
+	@Autowired
 	private EventRepository eventRepository;
 	
-	public String getAllEvents() {
-		return "This gets all of the events";
+	
+	public List<Event> getAllEvents() {
+		return this.eventRepository.findAll();
 	}
 	
 	public String getEventById(Long id) {
 		return String.format("This gets an event with ID %d", id);
 	}
 	
-	public String createEvent() {
-		return "This creates an event";
+	public Event createEvent(@Valid CreateEventDTO data) {
+		Event newEvent = modelMapper.map(data, Event.class);
+		return this.eventRepository.save(newEvent);
 	}
 	
 	public String updateEventById(Long id) {
