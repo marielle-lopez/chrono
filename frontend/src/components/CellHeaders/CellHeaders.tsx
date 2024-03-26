6d2ca80cafc688546b04812079454b95d26e8021
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-26 11:54:45 +1100

    add font styling and slice string

--- frontend/src/components/CellHeaders/CellHeaders.tsx
+++ frontend/src/components/CellHeaders/CellHeaders.tsx
@@ -12,7 +12,9 @@ const CellHeaders = () => {
   return (
     <div className="grid grid-cols-7">
       {daysOfWeek.map((day) => (
-        <p key={day}>{day}</p>
+        <p key={day} className="font-medium uppercase text-sm tracking-wide">
+          {day.slice(0, 3)}
+        </p>
       ))}
     </div>
   );
