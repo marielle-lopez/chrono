2cdf5d059e2dd3a06c774d892031b9a5eb9e795a
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-26 13:27:41 +1100

    create instantiation of `EventService` and utilise service layer for available request methods

--- backend/src/main/java/marielle/lopez/chrono/events/EventController.java
+++ backend/src/main/java/marielle/lopez/chrono/events/EventController.java
@@ -1,8 +1,10 @@
 package marielle.lopez.chrono.events;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,23 +12,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/events")
 public class EventController {
+	@Autowired
+	private EventService eventService;
+	
 	@GetMapping
 	public String getAllEvents() {
-		return "This gets all events";
+		return this.eventService.getAllEvents();
+	}
+	
+	@GetMapping("/{id}")
+	public String getEvent(@PathVariable Long id) {
+		return this.eventService.getEventById(id);
 	}
 	
 	@PostMapping
 	public String createEvent() {
-		return "This creates an event";
+		return this.eventService.createEvent();
 	}
 	
-	@PatchMapping
-	public String updateEvent() {
-		return "This updates an event";
+	@PatchMapping("/{id}")
+	public String updateEvent(@PathVariable Long id) {
+		return this.eventService.updateEventById(id);
 	}
 	
-	@DeleteMapping
-	public String deleteEvent() {
-		return "This deletes an event";
+	@DeleteMapping("/{id}")
+	public String deleteEvent(@PathVariable Long id) {
+		return this.eventService.deleteEventById(id);
 	}
 }
