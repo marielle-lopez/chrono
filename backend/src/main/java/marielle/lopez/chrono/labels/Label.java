3f70befeb8ab394ec4498d2ae5ec076f68016c4a
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-27 14:07:40 +1100

    add `Table` annotation to ensure name of table in database is given argument

--- backend/src/main/java/marielle/lopez/chrono/labels/Label.java
+++ backend/src/main/java/marielle/lopez/chrono/labels/Label.java
@@ -2,9 +2,11 @@ package marielle.lopez.chrono.labels;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import marielle.lopez.chrono.common.BaseEntity;
 
 @Entity
+@Table(name = "labels")
 public class Label extends BaseEntity {
 	@Column
 	private String name;
