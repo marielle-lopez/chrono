b1eed7174f8e3ae64c99f27414d61054d17bca5a
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-27 17:45:01 +1100

    update `label` property to `labelId`

--- frontend/src/helpers/schemas.ts
+++ frontend/src/helpers/schemas.ts
@@ -8,7 +8,7 @@ export const eventSchema = z
     location: z
       .string()
       .min(1, { message: 'Must be at least 1 character long' }),
-    label: z.string().min(1, { message: 'Must be at least 1 character long' }),
+    labelId: z.coerce.number().int().gte(1),
   })
   .refine((data) => data.endedAt >= data.startedAt, {
     message: 'End date must be after or the same as start date',
