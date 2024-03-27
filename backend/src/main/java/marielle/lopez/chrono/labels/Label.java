8fb54c14e06755c7ba626d14cc56e1511b769926
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-27 17:55:54 +1100

    add `events` property to hold events with corresponding label, add getter and settter methods

--- backend/src/main/java/marielle/lopez/chrono/labels/Label.java
+++ backend/src/main/java/marielle/lopez/chrono/labels/Label.java
@@ -1,9 +1,15 @@
 package marielle.lopez.chrono.labels;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import marielle.lopez.chrono.common.BaseEntity;
+import marielle.lopez.chrono.events.Event;
 
 @Entity
 @Table(name = "labels")
@@ -11,15 +17,27 @@ public class Label extends BaseEntity {
 	@Column
 	private String name;
 	
-	public String getName() {
-		return this.name;
-	}
+	@OneToMany(mappedBy = "label")
+	@JsonIgnoreProperties("label")
+	private List<Event> events;
 	
 	public Label() {
 		super();
 	}
 	
+	public String getName() {
+		return this.name;
+	}
+	
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	public List<Event> getEvents() {
+		return this.events;
+	}
+	
+	public void setEvents(List<Event> events) {
+		this.events = events;
 	}
 }
