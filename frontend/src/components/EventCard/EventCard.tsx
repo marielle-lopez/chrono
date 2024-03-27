41679b87029afa4f05b78661cbe658ecc9b1b842
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-03-27 21:27:28 +1100

    format time, update conditions for styling

--- frontend/src/components/EventCard/EventCard.tsx
+++ frontend/src/components/EventCard/EventCard.tsx
@@ -9,24 +9,24 @@ const EventCard = ({
   let textStyles: string = '';
 
   const label = () => {
-    if (event.label === 'personal') {
+    if (event.label.id === 2) {
       containerStyles =
-        'bg-lime-400 bg-opacity-20 px-2 py-1 rounded-tr-lg rounded-br-lg border-l-4 border-lime-400 hover:bg-black';
+        'bg-lime-400 bg-opacity-20 px-2 py-1 rounded-tr-lg rounded-br-lg border-l-4 border-lime-400 hover:bg-opacity-30';
       textStyles = 'text-lime-200 font-medium text-sm';
       return;
-    } else if (event.label === 'work') {
+    } else if (event.label.id === 3) {
       containerStyles =
-        'bg-teal-400 bg-opacity-20 px-2 py-1 rounded-tr-lg rounded-br-lg border-l-4 border-teal-400 hover:bg-black';
+        'bg-teal-400 bg-opacity-20 px-2 py-1 rounded-tr-lg rounded-br-lg border-l-4 border-teal-400 hover:bg-opacity-30';
       textStyles = 'text-teal-200 font-medium text-sm';
       return;
-    } else if (event.label === 'university') {
+    } else if (event.label.id === 4) {
       containerStyles =
-        'bg-violet-400 bg-opacity-20 px-2 py-1 rounded-tr-lg rounded-br-lg border-l-4 border-violet-400 hover:bg-black';
+        'bg-violet-400 bg-opacity-20 px-2 py-1 rounded-tr-lg rounded-br-lg border-l-4 border-violet-400 hover:bg-opacity-30';
       textStyles = 'text-violet-200 font-medium text-sm';
       return;
     } else {
       containerStyles =
-        'bg-blue-400 bg-opacity-20 px-2 py-1 rounded-tr-lg rounded-br-lg border-l-4 border-blue-400 hover:bg-black';
+        'bg-blue-400 bg-opacity-20 px-2 py-1 rounded-tr-lg rounded-br-lg border-l-4 border-blue-400 hover:bg-opacity-30';
       textStyles = 'text-blue-200 font-medium text-sm';
       return;
     }
@@ -38,7 +38,9 @@ const EventCard = ({
     <div className={containerStyles} onClick={() => setEvent(event)}>
       <p className={textStyles}>{event.name}</p>
       <p className="uppercase text-xs font-semibold opacity-30">
-        {event.startedAt.toLocaleTimeString()}
+        {event.startedAt.toLocaleTimeString([], {
+          timeStyle: 'short',
+        })}
       </p>
     </div>
   );
